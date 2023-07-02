0fcfc8730581c7b889dd84697686e1c128bbc58c
ernestpworrell <36313089+1960s@users.noreply.github.com>
2023-07-02 15:53:49 -0500

    update

--- question/questions.js
+++ question/questions.js
@@ -1,73 +1,45 @@
 CATEGORIES
-Intro
-Use mathematician guide
-POSSIBILITY SPACE, RANGE: Use all possible outcomes,generate possibility space and resolution, parameters
-PROBABILITY SPACE, RANGE: interpreting the value of possibility, act according to new information but common streams
-	Next step is Filter and byproducts, rule creation, restrictions, what can and cannot be, parameters
-	also conditional changes
-	
-What if opposite
 
-Analogy
-Diagram
-Explanation
-Picture
-Technical
+INTRO
+ GOAL/CONDITION DEFINITION
+ BREAKING INTO TRANSFORMATIONS/TASKS,SUBTASKS,MILESTONES,	series of conditions/states/goods
+ RESEARCH mechanics science
+	POSSIBILITY SPACE, RANGE: Use all possible outcomes, generate possibility space and resolution, parameters, variables
+	PROBABILITY SPACE, RANGE: interpreting the value of possibility, act according to new information but common streams
+		Next step is Filter and byproducts, rule creation, restrictions, what can and cannot be, parameters
+		also conditional changes
+ DESIGN control flow, to condition
+ GATHER tools suppliers, storage and transport/using frameworks or building from scratch, that is low vs high level
+ BUILD
+	TRANSFORMATIONS, CHANGE
+	 => goal/condition to meet, break down into sub goals milestones
+	let relationship = [
+	material, information, energy,
+	ingredients, recipe, work, result (tools that form content vs the content itself)
+	objects/data, functions, processing, return value
+	nouns/assets, verbs/act, actor, 
+	goods, services, servicer, condition
+	tools, techniques, energy/effort, goal
+	body, mind, spirit/efficient, soul, 
+	potientalizer => Potential => actual => actualizer/potentializer
+	tool-maker => tool => technique => techniquer
+	knife-maker, knife, slice, chef/slicer
+	sword-maker, sword, swording, swordsmen,  
+	]
+	condition-maker => must continually maintain the condition, greater difference, greater decay rate
+	maintenance is to rebuild contrary to the natural condition. The greater the difference the faster the decay.
+	fighting a natural condition, greater opposition, greater maintenance required
 
-Summary
-Grand scheme relationship
+---
+<textarea hidden id="relationship">
+hierarchy (is a), parent ,child, and sibilings
+part (has a)
+scale (???)
+tool-maker, tool
+technique-maker, technique
+potential or actual, good or service
+</textarea>
 
-Resources Types
-Energy information material
-
-Tool Types
-storage and transport
-
-Level types
-low and high
-structural vs superficial
-
-
-Science how it works
-Engineer making it do things
-
-```mermaid
-graph LR
-RESEARCH---
-DESIGN---
-BUILD---
-OPERATE
-```
-
-let analogyTypes = [
-"fruits",
-"foods",
-"animals",
-"colors",
-"martial arts",
-"weapons",
-"cars",
-chemistry
-]
-
-TRANSFORMATIONS, CHANGE
-let relationship = [
-potientalizer => Potential => actual => actualizer
-tool-maker => tool => technique => techniquer
-knife-maker, knife, slice, chef/slicer
-sword-maker, sword, swording, swordsmen,  
-]
-body, mind, spirit/efficient, soul, 
-Potential/capable, service, power source, and goal/motive
-condition-maker => must continually maintain the condition, greater difference, greater decay rate
-maintenance is to rebuild contrary to the natural condition. The greater the difference the faster the decay.
-fighting a natural condition, greater opposition, greater maintenance required
-
-objects/data and functions
-nouns and verbs
-goods and services 
-series of conditions/states/goods
-conditions can be created through understanding the mechanics
 
 
 
