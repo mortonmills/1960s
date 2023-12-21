838ba9e1aad7918e33397ddbf108859aa2c2b4dd
ernestpworrell <36313089+1960s@users.noreply.github.com>
2023-12-20 23:50:00 -0600

    add backup script

---
+++ notes/backup-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo add-apt-repository ppa:dobey/redshift-daily && sudo apt-get update && sudo apt-get install redshift vlc code git gh gtypist vim emacs ghex libreoffice audacity audacious inkscape gimp simplescreenrecorder kolourpaint
+
+# make desktop same as home
+# node
+# nvm
+# npm i zx nodemon jsdoc zx -g
+# reaper
+# kapeli offline docs
+# frescobaldi
+# midi editor
+# catfish
+# mousepad
+# pandoc
